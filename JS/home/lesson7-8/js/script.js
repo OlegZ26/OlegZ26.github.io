f1983eecd61fc82eff04e0444dc0861e6c0b9323
OlegZ26 <olegzigu@gmail.com>
2015-12-28 09:31:39 +0200

    lesson7-8 add helpTool

--- JS/home/lesson7-8/js/script.js
+++ JS/home/lesson7-8/js/script.js
@@ -2,25 +2,24 @@ $(function() {
 
   $('.selectTabs li a').each(function (i) {
     $('.selectTabs li a:eq('+ i +')').on('click', function(e) {
-      e.preventDefault();
       $('.selectTabs li a').removeClass('active');
       $(this).addClass('active');
       $('.content div').hide();
-      $('.tab' + (i + 1)).show();    
+      $('.tab' + (i + 1)).show();
+      e.preventDefault();
     })
   });
 
   $('.formLines input').hover(
     function() {
-    $(this).next('em').stop(true, true).animate({opacity: "show"}, 700);
+    $(this).next('.helpTool').stop(true, true).animate({opacity: "show"}, 700);
   }, 
    function() {
-    $(this).next('em').hide();
+    $(this).next('.helpTool').hide();
   });
 
-  // $('.forms-place button').on('click', function() {
-  //   $('.formLines em').show();
-  //   return false;
-  // }), 
+  $('button').on('click', function() {
+    $('.helpTool').show();
+  }); 
 
 });
