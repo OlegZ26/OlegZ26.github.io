5354ea04b968a643a0f365c4a7964c09ef08ed9a
OlegZ26 <olegzigu@gmail.com>
2016-02-24 12:42:05 +0200

    fix slider

--- JS/home/lesson19-20/dist/js/script.js
+++ JS/home/lesson19-20/dist/js/script.js
@@ -14,4 +14,72 @@ $(function() {
     $(this).children('.banner__title').toggleClass('banner__title--active');
   });
 
+  $("#owl-example").owlCarousel({
+    // Most important owl features
+        singleItem : true,
+        itemsScaleUp : false,
+
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+        rewindSpeed : 1000,
+
+        //Autoplay
+        autoPlay : true,
+        stopOnHover : false,
+
+        // Navigation
+        navigation : false,
+        navigationText : ["prev","next"],
+        rewindNav : true,
+        scrollPerPage : false,
+
+        //Pagination
+        pagination : true,
+        paginationNumbers: false,
+
+        // Responsive
+        responsive: true,
+        responsiveRefreshRate : 200,
+        responsiveBaseWidth: window,
+
+        // CSS Styles
+        baseClass : "owl-carousel",
+        theme : "owl-theme",
+
+        //Lazy load
+        lazyLoad : false,
+        lazyFollow : true,
+        lazyEffect : "fade",
+
+        //Auto height
+        autoHeight : false,
+
+        //JSON
+        jsonPath : false,
+        jsonSuccess : false,
+
+        //Mouse Events
+        dragBeforeAnimFinish : true,
+        mouseDrag : true,
+        touchDrag : true,
+
+        //Transitions
+        transitionStyle : false,
+
+        // Other
+        addClassActive : false,
+
+        //Callbacks
+        beforeUpdate : false,
+        afterUpdate : false,
+        beforeInit: false,
+        afterInit: false,
+        beforeMove: false,
+        afterMove: false,
+        afterAction: false,
+        startDragging : false,
+        afterLazyLoad : false
+  });
+
 });
